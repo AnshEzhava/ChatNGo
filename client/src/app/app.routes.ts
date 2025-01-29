e8ca6ad6a3ca92b2fdb105745cea81b428490acb
Ansh Ezhava <anshdpsg@gmail.com>
2025-01-29 11:35:18 +0530

    Added chat page and some design changes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ChatComponent } from './pages/chat/chat.component';
 
 export const routes: Routes = [
   {
     path: 'welcome',
     component: WelcomeComponent,
+  },
+  {
+    path: 'chat',
+    component: ChatComponent,
   },
 ];
